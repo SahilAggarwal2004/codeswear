e145357a6e0df570c3312636c5eee5ae745ae888
Sahil <aggarwalsahil2004@gmail.com>
2022-04-09 20:42:29 +0530

    commit

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,15 +5,15 @@ import { AiOutlineShoppingCart } from 'react-icons/ai' // react-icons is a very 
 
 export default function Navbar() {
     return (
-        <nav className="flex flex-col justify-center items-center md:flex-row md:justify-start py-2 w-full relative">
+        <nav className="flex flex-col justify-center items-center md:flex-row md:justify-start py-2 w-full relative shadow-lg mb-4">
             <div className="mx-5 flex">
-                <Image src="/logo.png" alt="CodesWear" width={49} height={35} quality={100} />
+                <Link passHref href='/'><a><Image src="/logo.png" alt="CodesWear" width={49} height={35} quality={100} /></a></Link>
             </div>
             <ul className='flex space-x-4 font-semibold mt-2 mb-1 md:my-0'>
-                <Link passHref href='/'><a>Tshirts</a></Link>
-                <Link passHref href='/'><a>Hoodies</a></Link>
-                <Link passHref href='/'><a>Mugs</a></Link>
-                <Link passHref href='/'><a>Stickers</a></Link>
+                <Link passHref href='/categories/tshirts'><a>Tshirts</a></Link>
+                <Link passHref href='/categories/hoodies'><a>Hoodies</a></Link>
+                <Link passHref href='/categories/mugs'><a>Mugs</a></Link>
+                <Link passHref href='/categories/stickers'><a>Stickers</a></Link>
             </ul>
             <div className='mx-5 h-full absolute top-4 right-0 flex md:items-center md:top-0'>
                 <AiOutlineShoppingCart className='scale-150' />
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
+import { useRouter } from 'next/router'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
   return <>
-    <Navbar />
+    {router.pathname !== "/_error" && <Navbar />}
     <Component {...pageProps} />
-    <Footer />
+    {router.pathname !== "/_error" && <Footer />}
   </>
 }
 
